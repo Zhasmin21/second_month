5c0825e61e559e659f4a40088cad9c75405fdc0c
Zhasmin <alievazhasmin6@gmail.com>
2022-03-09 19:54:14 +0300

    gr

---
+++ lesson2/main.js
@@ -0,0 +1,86 @@
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// console.log(arr[8])
+// const mass = ["a", "b","c", "d", "e"];
+// console.log(mass[2])
+//  let obj = {
+//     username: 'join',
+//      followers: [
+//          {
+//               username: "Join"
+//          },
+//          {
+//               username: "Jasmin",
+//              followers: [
+//                  {
+//                       username: 'jack'
+//                  },
+//                  {
+//                       username: 'joe'
+//                  },
+//                  {
+//                       username: 'john'
+//                  }
+//              ]
+//          },
+//          {
+//               username: 'joe'
+//          }
+//      ]
+// }
+// console.log(obj.followers[1].followers[2].username)
+
+// const array = [
+//     {
+//         username: "Jack",
+//         full_name: "Jack Kyrgyz"
+//     },
+//     {
+//         username: "Joe",
+//         full_name: "Joe Kyrgyz"
+//     },
+//     {
+//          username: "Vito",
+//         full_name: "Vito Kyrgyz"
+//     },   {
+//          username: "Vito",
+//         full_name: "Vito Kazakh"
+//     }
+// ];
+// console.log(array[0].username, array[0].full_name)
+// console.log(array[1].username, array[1].full_name)
+// console.log(array[2].username, array[2].full_name)
+//
+// for( let i=0; i<array.length; i++) {
+//     console.log(array[i])
+// // }
+
+// let i = 1
+// while (i < 5){
+//     console.log("Hello");
+//     i++
+// }
+
+//
+// const users = [
+//     {
+//         username: "Jack",
+//         salary: 500
+//     },
+//     {
+//         username: "Joe",
+//         salary: 5000
+//     },
+//     {
+//         username: "Vito",
+//         salary: 1000
+//     }
+// ];
+// const filteredUsers = [];
+//
+// for(let user of users){
+//     if(user.salary > 500){
+//         filteredUsers.push(user)
+//     }
+// }
+// console.log(filteredUsers)
+
