337ed622ffa8b06291d81ab2b5b95f54b734a2fd
Zhasmin <alievazhasmin6@gmail.com>
2022-03-24 18:36:14 +0300

    dz6

---
+++ lesson6/dz6/main.js
@@ -0,0 +1,28 @@
+const massive =[
+    massive2 = [1,3,5,6,8,4,6,7],
+    massive3 = [5,3,10,6,8,41,6,78],
+    massive4 = [8,12,5,16,8,87,6,21],
+    massive5 = [85,95,5,54,8,4,33,7],
+    massive6 = [2,7,45,69,58,14,0,22]
+]
+
+ const obj = {
+    key1: 'value',
+
+    key2: 'value',
+
+    key3: 'value',
+
+    key4: 'value',
+
+    key5: 'value'
+}
+const obj2 = {
+    ...obj,
+    key1: "value",
+    key2: "value",
+    key3: "value"
+}
+
+
+
--- lesson6/es.js
+++ lesson6/es.js
@@ -16,6 +16,7 @@
 
 // console.log(`${first_name} ${last_name}`);  // es6
 
+
 // const getFullName = (user) => {
 //     // return "first_name: " + user.first_name + "\n" + "last_name: " + user.last_name;
 //     return `first name: ${user.first_name} \n last name ${user.last_name}`
@@ -26,12 +27,54 @@
 // }
 // console.log(getFullName(user))
 
-const mass = [1,2,3,4]
-const mass2 =[...mass]
 
-console.log(mass2)
+// const mass = [1,5,45,88]
+// const mass2 =[...mass]
+//
+// console.log(mass2)
 
-for (let i=0; i < mass.length; i ++){
-    mass.push(mass[i])
+// for (let i=0; i < mass.length; i ++){
+//     mass.push(mass[i])
+// }
+// console.log(mass2)
+
+
+
+// const obj = {
+//     usermame: "jack",
+//     age: 19,
+//     last_name: "barbaro",
+//     full_name: "jack barbaro"
+// }
+//
+// const obj2 = {
+//     ...obj,
+//     key: "value",
+//     key2: "value2"
+// }
+//
+// console.log(obj2)
+
+
+//деструктиризация
+
+const props = {
+    onsubmit: () => {
+        console.log("Submit")
+    },
+    data: [
+        {
+             key: 'value'
+        },
+        {
+             key: 'value'
+        },
+        {
+             key: 'value'
+        }
+    ]
 }
-console.log(mass2)
+const {data,onsubmit} = props;
+
+console.log(data)
+onsubmit();
